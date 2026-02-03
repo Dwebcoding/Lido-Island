c4cb09cd091dfb31333bedde9c090179b0360902
Dwebcoding <TheEmperorNewGroove@proton.me>
2026-02-03 19:54:14 +0100

    Debug: Log dati inviati a EmailJS

--- js/booking.js
+++ js/booking.js
@@ -619,6 +619,7 @@ function sendBookingEmail(booking) {
         };
         
         console.log('[Email] Invio a EmailJS API...');
+        console.log('[Email] Dati inviati:', JSON.stringify(emailData, null, 2));
         
         // Chiama l'API REST di EmailJS
         fetch('https://api.emailjs.com/api/v1.0/email/send', {
